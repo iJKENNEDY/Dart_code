c9125c0e17d19af70928a577367062782e463df9
iJKENNEDY <wall.kennedy@hotmail.com>
2020-11-30 18:55:05 -0500

    --no-edit

---
+++ src/_101_flowControl/variables.dart
@@ -0,0 +1,19 @@
+var author = "Turing 03";
+var numberFavorite = 777;
+var pi = 3.1415;
+var fruits = ['Orange', 'Lemon', 'Watermelon', 'Banana'];
+var Contacts_ = {'name': 'Orland', 'level': 2, 'city': 'NO'};
+
+void main() {
+  print(author);
+  print(numberFavorite);
+  print(pi);
+
+  print(".........");
+
+  for (var item in fruits) {
+    print(item);
+  }
+  print("..........")
+  print(Contacts_);
+}
