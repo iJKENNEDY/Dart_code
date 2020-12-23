9577b6493bf7fc6af9e34f824fe6e8bc56a9e182
iJKENNEDY <wall.kennedy@hotmail.com>
2020-12-22 23:57:20 -0500

    notas variable

--- src/_101_flowControl/variables.dart
+++ src/_101_flowControl/variables.dart
@@ -3,6 +3,24 @@ var numberFavorite = 777;
 var pi = 3.1415;
 var fruits = ['Orange', 'Lemon', 'Watermelon', 'Banana'];
 var Contacts_ = {'name': 'Orland', 'level': 2, 'city': 'NO'};
+var name = "Sherlock";
+dynamic firtsName = "Holmes";
+String dog = "pow pow";
+
+// default value
+int lineCount;
+
+// final-> is initialized
+final fruits_ = 'Orange';
+final String fruitsColor = "Orange";
+
+//
+const bar = 1000000;
+const double atm = 3.2333 * bar;
+
+var foo = const [];
+final bar3 = const [];
+const baz = [];
 
 void main() {
   print(author);
@@ -14,6 +32,9 @@ void main() {
   for (var item in fruits) {
     print(item);
   }
+
+  print(lineCount);
+
   print("..........");
   print(Contacts_);
 }
